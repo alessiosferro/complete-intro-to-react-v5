332a097d8b5f947db5f71ab98723b81ba03a5afe
Alessio Sferro <sferro.alessio@gmail.com>
2020-09-28 19:51:58 +0200

    add npm scripts and eslint and prettier

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Pet = ({ animal, name, breed }) =>
+  React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed),
+  ]);
+
+const App = () =>
+  React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, {
+      name: 'Doink',
+      animal: 'Cat',
+      breed: 'Stray',
+    }),
+  ]);
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
